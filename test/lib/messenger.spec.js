9fc76622a80e005b73a1b548f229cc0b9816573f
peterdemartini <thepeterdemartini@gmail.com>
2018-06-26 16:00:38 -0700

    WIP: test messenger

--- test/lib/messenger.spec.js
+++ test/lib/messenger.spec.js
@@ -3,11 +3,26 @@
 const Messenger = require('../../lib/messenger');
 
 describe('Messenger', () => {
-    describe('when connected without a host', () => {
+    describe('when constructed without a host', () => {
         it('should throw an error', () => {
             expect(() => {
                 new Messenger(); // eslint-disable-line
             }).toThrowError('Messenger requires a valid host');
+        });
+    });
+
+    describe('when constructed with an invalid host', () => {
+        let messenger;
+        beforeEach(() => {
+            messenger = new Messenger('http://idk.example.com', {
+                timeout: 1000,
+                reconnection: false,
+            });
+        });
+
+        it('connect should throw an error', () => {
+            const errMsg = 'Unable to connect: Error: xhr poll error';
+            return expect(messenger.connect()).rejects.toThrowError(errMsg);
         });
     });
 });
